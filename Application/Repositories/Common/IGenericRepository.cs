4fe8133f6d735f17823c5279f189964ed9037118
Serjux <36546740+Serjux@users.noreply.github.com>
2023-03-06 17:26:30 +0000

    IGenericRepository namespace cleanup.

--- Application/Repositories/Common/IGenericRepository.cs
+++ Application/Repositories/Common/IGenericRepository.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using Domain.Entities.Common;
 
-namespace Application.Repositories.Common
+namespace Application.Repositories.Common;
+
+public interface IGenericRepository<TEntity> where TEntity : BaseEntity
 {
-	public interface IGenericRepository<TEntity> where TEntity : BaseEntity
-	{
-		Task<TEntity?> GetById(Guid id);
+	Task<TEntity?> GetById(Guid id);
 
-		Task<TEntity> Insert(TEntity entity);
+	Task<TEntity> Insert(TEntity entity);
 
-		Task<TEntity> Update(TEntity entity);
+	Task<TEntity> Update(TEntity entity);
 
-		Task Delete(TEntity entity);
-	}
+	Task Delete(TEntity entity);
 }
-
